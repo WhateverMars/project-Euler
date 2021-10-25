62467eedb89b99a3e4ebdda591f7d9f6ec408c08
David Kelly <davekelly1224@gmail.com>
2021-10-25 14:36:38 +0100

    improved run speed of problem 7 solution

--- 7-10001st-prime.py
+++ 7-10001st-prime.py
@@ -1,7 +1,9 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 #What is the 10001st prime number?
+import time
 
+st_time = time.time()
 
 # this test function checks to see if there exists any factors between 1 and num not inclusive. If so, then num is not prime.
 def is_prime(num):
@@ -9,11 +11,21 @@ def is_prime(num):
     # prime numbers cannot be below 2
     if num < 2:
         return False
-    
-    # we are looking to see if there are any factors other than 1 and num, if so then not prime
-    for i in range( 2, num):
-        if num%i == 0:
+
+    # special cases for 2 and 3 allows us to iterate by 2 in the future. No even primes other than 2
+    if num == 2: 
+        return True
+
+    if num == 3:
+        return True
+
+    # we are looking to see if there are any factors other than 1 and itself, if so then not prime   
+    i = 3
+    while i < num:
+        if num%i == 0: 
             return False
+        i += 2
+
 
     return True
 
@@ -25,12 +37,18 @@ def nth_prime(n):
 
     # repeat until the count reaches the nth prime
     while count < n:
-
         if is_prime(i):
             count += 1
+            #print('primes found: '+str(count))
             if count == n:
                 return i
-        i += 1
+
+        # can increment by 2 as all prime numbers above 2 are odd
+        if i<3:
+            i += 1
+        else:
+            i += 2
     
 
 print(nth_prime(10001))
+print('time: '+ str(time.time()-st_time))
